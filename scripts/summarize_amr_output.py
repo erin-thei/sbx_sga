8c8ab9db107e0f3b753d4f3ce061d96553c180eb
erin-thei <erin.theiller@temple.edu>
2025-03-19 15:18:05 -0400

    Add summary rules for bakta, abritamr, shovill and checkm

---
+++ scripts/summarize_amr_output.py
@@ -0,0 +1,65 @@
+#!/usr/local/bin/python3 
+
+import csv
+
+input_reports = snakemake.input
+output_report = snakemake.output[0]
+
+AMR_dict = {}
+
+subclassList = {}
+for file in input_reports:
+    sample = file.split('/')[-2]
+    file_obj = open(file, 'r')
+    filelines = file_obj.readlines()
+    for line in filelines[1:]:
+        line = line.rstrip()
+        line_list = line.split('\t')
+        scope = line_list[8]
+        gene = line_list[5]
+                        
+        if scope == 'AMR':
+            subclass = line_list[11]
+            if subclass not in subclassList:
+                subclassList[subclass] ={}
+            if sample not in subclassList[subclass]:
+                subclassList[subclass][sample] = [gene]
+            else:
+                subclassList[subclass][sample].append(gene)
+            if sample in AMR_dict:
+                AMR_dict[sample].append(gene)
+            else:
+                AMR_dict[sample] = [gene]
+        else:
+            continue
+
+# Extracting all sample names and subclass keys
+samples = set()
+subclasses = set(subclassList.keys())
+
+for subclass in subclassList:
+    samples.update(subclassList[subclass].keys())
+
+# Preparing data structure for TSV
+tsv_data = {}
+for sample in samples:
+    tsv_data[sample] = {subclass: '' for subclass in subclasses}
+
+    for subclass in subclasses:
+        if sample in subclassList[subclass]:
+            tsv_data[sample][subclass] = '\t'.join(subclassList[subclass][sample])
+
+# Writing to a file
+with open(output_report, 'w', newline='') as tsv_file:
+    writer = csv.writer(tsv_file, delimiter=',')
+
+    # Writing the header
+    header = ['Sample'] + list(subclasses)
+    writer.writerow(header)
+
+    # Writing the rows
+    for sample, subclass_dict in tsv_data.items():
+        row = [sample] + [subclass_dict[subclass] for subclass in subclasses]
+        writer.writerow(row)
+
+
---
+++ scripts/summarize_checkm.py
@@ -0,0 +1,24 @@
+#!/usr/local/bin/python3 # Do I need to change this?
+
+import sys 
+
+input_reports = snakemake.input 
+output_report = open(snakemake.output[0], 'w')
+output_report.write('Sample,Completeness,Contamination,N50,Genome_Size,GC_Content,CDS\n')
+
+for file in input_reports:
+    file_obj = open(file, 'r')
+    filelines = file_obj.readlines()
+    for line in filelines[1:]: # Skip header
+        line = line.rstrip()
+        line_list = line.split('\t')
+        sample = line_list[0]
+        completeness = line_list[1]
+        contamination = line_list[2]
+        n50 = line_list[6]
+        genome_size = line_list[8]
+        gc_content = line_list[9]
+        cds = line_list[10]
+    output_report.write(f'{sample},{completeness},{contamination},{n50},{genome_size},{gc_content},{cds}\n')
+
+output_report.close()
